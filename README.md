a4eed9f8da5b7b9344d37a0a1e7538be2daeeef9
williamburger <burgerw@williams-mbp.wireless.dartmouth.edu>
2017-05-16 15:34:14 -0400

    finished with lab

---
+++ README.md
@@ -0,0 +1,1 @@
+yay workshop for ios react-native!
---
+++ youtube-api.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+
+const API_URL = 'https://www.googleapis.com/youtube/v3/search';
+const API_KEY = 'AIzaSyDQKD_bmcn1_vL74jAOKjQ0HKShPWqrFvs';
+
+const youtubeSearch = (term) => {
+  const params = {
+    part: 'snippet',
+    key: API_KEY,
+    q: term,
+    type: 'video',
+  };
+
+  return new Promise((resolve, reject) => {
+    axios.get(API_URL, { params })
+      .then((response) => {
+        resolve(response.data.items);
+      })
+      .catch((error) => {
+        console.log(`youtube api error: ${error}`);
+        reject(error);
+      });
+  });
+};
+
+export default youtubeSearch;
